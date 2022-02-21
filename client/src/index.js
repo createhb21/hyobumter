21324adc20d86034e60d06e6a177552bedb9cd20
createhb21 <alsghk9701@gmail.com>
2022-02-22 01:56:54 +0900

    client fetch modules

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HttpClient from './network/http';
 import AuthService from './service/auth';
 import TweetService from './service/tweet';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,9 +10,10 @@ import { AuthProvider } from './context/AuthContext';
 import { AuthErrorEventBus } from './context/AuthContext';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
+const httpClient = new HttpClient(baseURL);
 const authErrorEventBus = new AuthErrorEventBus();
 const authService = new AuthService();
-const tweetService = new TweetService(baseURL);
+const tweetService = new TweetService(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
