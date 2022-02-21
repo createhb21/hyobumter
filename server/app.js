7a60e520cc73459cbb71f9e3c5fab42bf58ba416
createhb21 <alsghk9701@gmail.com>
2022-02-21 18:45:58 +0900

    CRUD system

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import "express-async-errors";
-import tweetsRouter from "./router/tweets";
+import tweetsRouter from "./router/tweets.js";
 
 const app = express();
 app.use(express.json());
-app.use(usehelmet());
+app.use(helmet());
 app.use(cors());
 app.use(morgan("tiny"));
 
